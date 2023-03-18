cd886283ccf9b2c848320b60f084c76b6730ddd1
Dmitry-Boyko <dmt.b@yahoo.com>
2023-03-18 13:18:12 -0400

    passing variable value into text file

--- week2/answers/practice3.js
+++ week2/answers/practice3.js
@@ -1,10 +1,10 @@
 const fs = require("fs").promises;
-const sentence = require("./practice2.js");
+const {sentence} = require("./practice2.js");
 
 const myFile = async () => {
   try {
-    const text = await fs.readFile("./practice2.js/","utf8");
-    await fs.writeFile("./content/practice2.txt", text);
+    //const text = await fs.readFile(`./practice2.js`,"utf8");
+    await fs.writeFile("./content/practice2.txt", sentence);
   } catch (err) {
     console.error(err);
   }
