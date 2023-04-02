07ec590e28033a9c9b0c954315f3f1652c81b631
Dmitry-Boyko <dmt.b@yahoo.com>
2023-04-01 21:15:44 -0400

    plaing with put method

--- week4/routes/people.js
+++ week4/routes/people.js
@@ -34,8 +34,8 @@ const products = [
 ]
 const people = [
   { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
+  { id: 2, name: 'joy' },
+  { id: 3, name: 'dilan' },
   { id: 4, name: 'anna' },
   { id: 5, name: 'emma' },
 ]
