5d2da3f30fd8c71441d778c56a48e9846647fc5e
Dmitry-Boyko <dmt.b@yahoo.com>
2023-04-11 14:54:58 -0400

    wip-02-all-working

--- week-5-pr/app.js
+++ week-5-pr/app.js
@@ -1,30 +1,29 @@
 const express = require("express")
 const app = express()
-// const mongoose = require("mongoose")
-const port = 3000
+
+// middlware
+app.use(express.json())
+
+// adds the routs here
+const tasks = require('./routes/tasks')
+
 
 
 // routs
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Task Manager App')
 })
 
-// mongoose.connect(
-//   process.env.MONGO_URI, 
-//   {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true
-//   }
-// )
+app.use('/api/v1/tasks', tasks)
 
-// require("dotenv").config()
+// app.get('/api/v1/tasks')          - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
 
 
-// const studentSchema = new mongoose.Schema({
-//   roll_no: Number,
-//   name: String,
-//   year: Number,
-//   subjects: [String]
-// })
-
+const port = 3000
 app.listen(port, () => console.log(`Server is running ${port}`))
+
+// video stop on 25.03
