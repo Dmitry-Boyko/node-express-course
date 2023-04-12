0e6feca6947c043e0c3e31d2a4a46f48945321bb
Dmitry-Boyko <dmt.b@yahoo.com>
2023-04-12 15:48:13 -0400

    responding to the week 5 questions

--- week4/03-task-manager/starter/controllers/tasks.js
+++ week4/03-task-manager/starter/controllers/tasks.js
@@ -5,15 +5,19 @@ const getAllTasks = (req, res) => {
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({task})
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
 const getTask = (req, res) => {
   res.json({id:req.params.id})
 }
 
-const updateTask = (req, res) => {
+const updateTask = async (req, res) => {
   res.send('update task')
 }
 
