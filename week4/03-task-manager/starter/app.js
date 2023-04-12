afe6003b82ea68c10c38abb9a98a2679e5822ec4
Dmitry-Boyko <dmt.b@yahoo.com>
2023-04-12 14:02:51 -0400

    connected to db and provided schema sreation

---
+++ week4/03-task-manager/starter/app.js
@@ -0,0 +1,45 @@
+require('./db/connect')
+
+console.log('Task Manager App')
+const express = require("express")
+const app = express()
+
+
+// middlware
+app.use(express.json())
+
+// adds the routs here
+const tasks = require('./routers/tasks')
+const connectDB = require('./db/connect')
+
+require('dotenv').config()
+
+
+// routs
+app.get('/', (req, res) => {
+  res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')          - get all the tasks
+// app.post('/api/v1/tasks')        - create a new task
+// app.get('/api/v1/tasks/:id')     - get single task
+// app.patch('/api/v1/tasks/:id')   - update task
+// app.delete('/api/v1/tasks/:id')  - delete task
+
+
+const port = 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => console.log(`Server is running ${port} and DB connected`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// video stop on 1:30:20
+
+start()
