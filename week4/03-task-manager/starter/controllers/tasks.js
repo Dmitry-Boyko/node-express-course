4636cb84e43478f0c3b538ba91df5915ace3096f
Dmitry-Boyko <dmt.b@yahoo.com>
2023-04-13 13:25:54 -0400

    added deleteTask api

--- week4/03-task-manager/starter/controllers/tasks.js
+++ week4/03-task-manager/starter/controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({})
-    res.status(200).json({tasks})
+    res.status(200).json({tasks}) 
   } catch (error) {
     res.status(500).json({msg:error})
   }
@@ -36,8 +36,17 @@ const updateTask = async (req, res) => {
   res.send('update task')
 }
 
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const {id:taskID} = req.params
+    const task = await Task.findOneAndDelete({_id:taskID})
+    if(!task) {
+      return res.status(404).json({msg: `No task with id: ${taskID}`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
 }
 
 
